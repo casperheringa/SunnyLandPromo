95863387814973dd9fb305d82494b98ad93724e7
Casper Heringa <5be8141b@opayq.com>
2019-09-01 18:09:09 +0200

    player movement (still needs adjustments)

---
+++ Sunny-Land-Promo/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D _rigidbody;
+    [SerializeField]
+    private float _jumpVelocity = 20f;
+    [SerializeField]
+    private float _movementSpeed = 6;
+
+    private void Update()
+    {
+
+        // _rigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * _movementSpeed, 0);
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            _rigidbody.velocity = new Vector2(-_movementSpeed, _rigidbody.velocity.y);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            _rigidbody.velocity = new Vector2(_movementSpeed, _rigidbody.velocity.y);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _jumpVelocity);
+        }
+
+    }
+}
